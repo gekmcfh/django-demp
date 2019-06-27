524cdfb703ab97f502038abcce3f17ce52d3cd72
gek <rostergek@gmail.com>
2019-06-28 00:03:27 +0300

    hw

--- chap5/blog_project/blog/views.py
+++ chap5/blog_project/blog/views.py
@@ -1,3 +1,15 @@
+#blog/views.py
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView #new
 
+from .models import Post
+
+class BlogListView(ListView):
+    model = Post
+    template_name = 'home.html'
 # Create your views here.
+
+class BlogDetailView(DetailView): #new
+    model = Post
+    template_name = 'post_detail.html'
+
