9f41dabcbbfd638ae251b367bf64958f43280ee4
gek <rostergek@gmail.com>
2019-06-30 11:21:46 +0300

    hw

--- chap5/blog_project/blog/views.py
+++ chap5/blog_project/blog/views.py
@@ -1,6 +1,8 @@
 #blog/views.py
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView,UpdateView #new
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import  CreateView,UpdateView,DeleteView #new
+from django.urls import reverse_lazy #new
 
 from .models import Post
 
@@ -22,3 +24,8 @@ class BlogUpdateView(UpdateView): #new
     model = Post
     template_name = 'post_edit.html'
     fields = ['title', 'body']
+
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('home')
