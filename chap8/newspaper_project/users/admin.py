c41145536f4f1c02eb620b0b0c9954fddfdeafa1
gek <rostergek@gmail.com>
2019-07-03 23:31:19 +0300

    hw

--- chap8/newspaper_project/users/admin.py
+++ chap8/newspaper_project/users/admin.py
@@ -1,3 +1,14 @@
+#users/admin.py
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
+from .forms import CustomUserChangeForm,CustomUserCreationForm
+from .models import CustomUser
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+
+admin.site.register(CustomUser,CustomUserAdmin)
 # Register your models here.
