9a718bc758d0a3660ff35fbdcf68eab5cfe812c1
gek <rostergek@gmail.com>
2019-07-12 23:00:50 +0300

    hw

--- chap8/newspaper_project/newspaper_project/settings.py
+++ chap8/newspaper_project/newspaper_project/settings.py
@@ -31,7 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'users.apps.UsersConfig', #new
+    'users.apps.UsersConfig',
+    'pages.apps.PagesConfig', #new
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
