c71b44adcc22b47431ebe5f82e2431b56b221adc
ILYA <rostergek@gmail.com>
2019-07-24 10:22:52 +0300

    rest-chap2 works

--- rest_chap2/library_project/api/views.py
+++ rest_chap2/library_project/api/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from rest_framework import generics
 from books.models import Book
+from .serializers import BookSerializer
 
 class BookAPIView(generics.ListAPIView):
     queryset = Book.objects.all()
