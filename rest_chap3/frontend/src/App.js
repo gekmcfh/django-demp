d61415fcc56e769de83727dad31661eb598dc425
gek <rostergek@gmail.com>
2019-07-26 01:13:55 +0300

    страница 72; todo отображаются в react

--- rest_chap3/frontend/src/App.js
+++ rest_chap3/frontend/src/App.js
@@ -1,43 +1,40 @@
 // src/App.js
 import React, { Component } from 'react';
-
-const list = [
-    {
-        "id":1,
-        "title":"1st todo",
-        "body":"Learn Django Properly."
-    },
-    {
-        "id":2,
-        "title":"Second item",
-        "body":"Learn Python."
-    },
-    {
-        "id":3,
-        "title":"Learn HTTP",
-        "body":"It's important"
-    }
-]
+import axios from 'axios';
 
 class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { list };
+    state = {
+        todos: []
+    };
+
+    componentDidMount() {
+        this.getTodos();
+    }
+
+    getTodos() {
+        axios
+            .get('http://127.0.0.1:8000/api')
+            .then(res => {
+                this.setState({todos: res.data});
+            })
+            .catch(err => {
+                console.log(err);
+            });
     }
 
 
-render() {
-    return (
-        <div>
-            {this.state.list.map(item => (
-                <div key={item.id}>
-                    <h1>{ item.title }</h1>
-                    <p>{ item.body }</p>
-                </div>
-            ))}
-        </div>
+    render() {
+        return (
+            <div>
+                {this.state.todos.map(item => (
+                    <div key={item.id}>
+                        <h1>{item.title}</h1>
+                        <span>{item.body}</span>
+                    </div>
+                ))}
+            </div>
         );
     }
 }
-            
+
 export default App;
